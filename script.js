41fb16d070bb882c1c59c8d3b38a5a9b1d6cd38c
OneGiantTree <115727986+TheOneGiantTree@users.noreply.github.com>
2023-02-25 15:06:08 -0500

    add single round game function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Have computer select from rock, paper, scissor
 // Have user select rock, paper, scissor
 // Make sure user inputs are not case sensitive
-// Return a string that declares the winner
+// Compare userInput to compInput and declare a winner
 // 
 //
 //  PSEUDOCODE
@@ -16,21 +16,45 @@
 // let userChoice = prompt user rock paper scissor
 // return string back with .toLowerCase
 // 
-// 
-// 
-// 
+// compare compInput to userInput
+//  if same, declare tie
+//  if user == rock && comp == scissors, declare win
+//  if user == paper && comp == rock, declare win
+//  if user == scissor && comp == paper, declare win
+//  else, declare loss
 //  
 //  
+
+// Computer's turn
 function compTurn() {
-let getComputerChoice = ["rock", "paper", "scissor"]
+let getComputerChoice = ["rock", "paper", "scissors"]
 let compChoice = getComputerChoice[Math.floor(Math.random() * getComputerChoice.length)];
 return compChoice;
 }
-console.log(compTurn());
 
-
+// User's turn
 function userTurn(){
     let playerChoice = prompt("Make a selection.");
-    console.log(playerChoice.toLowerCase())
+    return (playerChoice.toLowerCase())
 }
-userTurn()
+
+// creates variables to store the results of comp / user turns
+let compInput = compTurn()
+console.log(compInput)
+let userInput = userTurn()
+console.log(userInput)
+
+// compare the results of both turns and determine a winner.
+function game(){
+    if (userInput === compInput){
+        return "Tie!"
+    }
+    else if ((userInput == "rock" && compInput == "scissors")||
+        (userInput == "paper" && compInput == "rock")||
+        (userInput == "scissors" && compInput == "paper")||
+        (userInput == "paper" && compInput == "rock")){
+            return "You win!"
+        }
+    else {return "You lose."}
+}
+console.log(game())
