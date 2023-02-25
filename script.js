758dd99afa2a9036730e300720f0f91ec54381ac
OneGiantTree <115727986+TheOneGiantTree@users.noreply.github.com>
2023-02-25 11:23:18 -0500

    add computer turn function

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-
+//  ALGORITHM
 // Have computer select from rock, paper, scissor
 // Have user select rock, paper, scissor
 // Make sure user inputs are not case sensitive
 // Return a string that declares the winner
 // 
-function getComputerChoice (rock, paper, scissor){
-
+//
+//  PSEUDOCODE
+// function getComputerChoice (choice){
+//     select random number 0-2
+//         if 0 let choice = rock
+//         if 1 let choice = paper
+//         if 2 let choice = scissor
+//      return choice
+// 
+// let userChoice = prompt user rock paper scissor
+// return string back with .toLowerCase
+// 
+// 
+// 
+// 
+//  
+//  
+function compTurn() {
+let getComputerChoice = ["rock", "paper", "scissor"]
+let compChoice = getComputerChoice[Math.floor(Math.random() * getComputerChoice.length)];
+console.log(compChoice)
 }
-console.log(getComputerChoice)
+compTurn()
