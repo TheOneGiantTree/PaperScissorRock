8a742d8ce9952ae2a4bb07cb84ec698cd6cbb956
OneGiantTree <115727986+TheOneGiantTree@users.noreply.github.com>
2023-02-25 08:43:45 -0500

    create index.html, script.js, and write algorithm

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Have computer select from rock, paper, scissor
+// Have user select rock, paper, scissor
+// Make sure user inputs are not case sensitive
+// Return a string that declares the winner
+// 
+function getComputerChoice (rock, paper, scissor){
+
+}
+console.log(getComputerChoice)
